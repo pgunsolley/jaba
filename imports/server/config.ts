0de8ca67dbc2a60eaec1edeb2d01f8bb8910bd00
Patrick Gunsolley <patrick.gunsolley@outlook.com>
2025-11-23 06:39:05 -0700

    Updated .env for dev; added dependency

--- imports/server/config.ts
+++ imports/server/config.ts
@@ -2,5 +2,4 @@ import env from 'env-var';
 
 export default {
     mediaPath: env.get('MEDIA_PATH').required().asString(),
-    thumbnailPathSuffix: env.get('THUMBNAIL_PATH_SUFFIX').required().asString(),
 };
